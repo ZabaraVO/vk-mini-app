e6fb77b810c84936c823c1c7ebf57c77cfd35d62
Vlad <vlad-z-alm@mail.ru>
2020-02-11 13:48:11 +0500

    Updates

---
+++ precache-manifest.9e250cc3bf3ede0cabcdbb48706f70b2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42cbf72365a94d923cb3f8b120d9007e",
+    "url": "app/index.html"
+  },
+  {
+    "revision": "71e96c50de1d8c039a64",
+    "url": "app/static/css/2.2cfa1b23.chunk.css"
+  },
+  {
+    "revision": "b552a02ee3d000b819da",
+    "url": "app/static/css/main.66c45bed.chunk.css"
+  },
+  {
+    "revision": "71e96c50de1d8c039a64",
+    "url": "app/static/js/2.ef3092cd.chunk.js"
+  },
+  {
+    "revision": "a1afa5f7c8dff2a316c41be2103336f5",
+    "url": "app/static/js/2.ef3092cd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b552a02ee3d000b819da",
+    "url": "app/static/js/main.9b345691.chunk.js"
+  },
+  {
+    "revision": "cd94fb94e3c2106e6723",
+    "url": "app/static/js/runtime-main.af45a017.js"
+  },
+  {
+    "revision": "bdea5aa60d2b9ecaef7a2877b36e040b",
+    "url": "app/static/media/sad.bdea5aa6.png"
+  },
+  {
+    "revision": "c8b601ab4ff675065ee970fe871d6502",
+    "url": "app/static/media/thumbup.c8b601ab.png"
+  }
+]);
