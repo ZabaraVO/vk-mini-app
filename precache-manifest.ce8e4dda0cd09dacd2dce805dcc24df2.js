1fd82fe4205e41f7ab8c0cdba493fa16ba8c7d86
Vlad <vlad-z-alm@mail.ru>
2020-03-16 19:00:18 +0500

    Updates

---
+++ precache-manifest.ce8e4dda0cd09dacd2dce805dcc24df2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69dd2a64e58ef813967fbef7638e6406",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8fc5cb345377562b41de",
+    "url": "./static/css/2.2cfa1b23.chunk.css"
+  },
+  {
+    "revision": "3f088d4c7942cb74c801",
+    "url": "./static/css/main.e86ae5db.chunk.css"
+  },
+  {
+    "revision": "8fc5cb345377562b41de",
+    "url": "./static/js/2.f6105457.chunk.js"
+  },
+  {
+    "revision": "21cccdc2186416e057a517afb61ae289",
+    "url": "./static/js/2.f6105457.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3f088d4c7942cb74c801",
+    "url": "./static/js/main.7fdcc809.chunk.js"
+  },
+  {
+    "revision": "52d94beb58dd5d9a2eca",
+    "url": "./static/js/runtime-main.6dca4322.js"
+  },
+  {
+    "revision": "34157c7f0d77e771f78cb2b4d72d9546",
+    "url": "./static/media/kind.34157c7f.png"
+  },
+  {
+    "revision": "7cc058fa4f04526d804c7ce31fb6bac2",
+    "url": "./static/media/sad.7cc058fa.png"
+  },
+  {
+    "revision": "677ed69484d18bb53fa25c49e5922b48",
+    "url": "./static/media/thumbup.677ed694.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.4c972e2d0e333a6830f202ac1947ffcd.js"
+  "./precache-manifest.ce8e4dda0cd09dacd2dce805dcc24df2.js"
 );
 
 self.addEventListener('message', (event) => {
